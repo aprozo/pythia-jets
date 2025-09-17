95b26fbe27fca3789efa69a028446509658b2102
aprozo <alexandr.a.prozorov@gmail.com>
2025-09-17 16:18:04 -0400

    typo

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apptainer exec -B /gpfs01 /gpfs01/star/pwg/prozorov/dijets/rivet-pythia/rivet-pythia.sif bash
+apptainer exec -B /gpfs01 /gpfs01/star/pwg/prozorov/dijets/pythia-jets/rivet-pythia.sif bash
